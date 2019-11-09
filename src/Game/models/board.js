1e86ff5515c6871c779c36549dbd3de5ad0c36d9
PamJez <pamela.pikula@gmail.com>
2019-11-09 22:45:33 +0100

    Implementation of queen moves

--- src/Game/models/board.js
+++ src/Game/models/board.js
@@ -1,5 +1,6 @@
 import Cat from "../pieces/cat";
 import Pawn from "../pieces/pawn";
+import Queen from "../pieces/queen";
 
 export default class Board extends Array {
 
@@ -11,10 +12,12 @@ export default class Board extends Array {
 
   // tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
   createAndSetPawns(side){
-    const row = side === "white" ? 6 : 1;
+    const row = side === "white" ? 7 : 0;
+    const rowOffset = side === "white" ? -1 : 1;
     for(let i = 0; i < 8; i++ ){
-      this[row][i] = new Pawn(row, i, side);
+      this[row+rowOffset][i] = new Pawn(row, i, side);
     }
+    this[row][3] = new Queen(row, 3, side);
   }
 
 
