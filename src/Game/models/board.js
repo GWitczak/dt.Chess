a44662474e4282553cd13430a12c9f20ea9ef04d
kacus <54528490+kacus@users.noreply.github.com>
2019-11-10 17:54:52 +0100

    Pawn update

--- src/Game/models/board.js
+++ src/Game/models/board.js
@@ -14,19 +14,19 @@ export default class Board extends Array {
   }
 
   // tutaj tworzycie nowe obiekty waszych bierek i od razu umieszczacie je na szachownicy
-  createAndSetPawns(side){
+  createAndSetPawns(side) {
     const row = side === "white" ? 6 : 1;
-    for(let i = 0; i < 8; i++ ){
+    for (let i = 0; i < 8; i++) {
       this[row][i] = new Pawn(row, i, side);
     }
   }
-  
-  createAndSetQueen(side){
+
+  createAndSetQueen(side) {
     const row = side === "white" ? 7 : 0;
     this[row][3] = new Queen(row, 3, side);
   }
 
-  createAndSetKnights(side){
+  createAndSetKnights(side) {
     const row = side === "white" ? 7 : 0;
     this[row][1] = new Knight(row, 1, side);
     this[row][6] = new Knight(row, 6, side);
--- src/Game/pieces/pawn.js
+++ src/Game/pieces/pawn.js
@@ -8,19 +8,50 @@ class Pawn extends Piece {
     this._vector = this._side == "white" ? -1 : 1; // 1 to góra -1 to dół
   }
 
-  filterOutBoardMoves(possibleMoves) {
-    return possibleMoves.filter( (x) => (x[0] >= 0 && x[0] < 8) && (x[1] >= 0 && x[1] < 8));
+  static filterOutBoardMoves(moves) {
+    // return only moves on correct possition on board
+    return moves.filter((move) => (move[0] >= 0 && move[0] < 8) && (move[1] >= 0 && move[1] < 8));
+  }
+
+  static filterOccupiedPossition(moves, board) {
+    // return only moves on free possition
+    return moves.filter((move) => (board[move[0]][move[1]] == undefined));
+  }
+
+  findLegalAttacks(board) {
+    //attacks of pawn
+    let attacks = [
+      [this._x + this._vector, this._y + 1],
+      [this._x + this._vector, this._y - 1]
+    ];
+    // filter out board attacks
+    attacks = Pawn.filterOutBoardMoves(attacks);
+    // return only attacks on enemy pieces
+    return attacks.filter((move) =>
+      board[move[0]][move[1]] != undefined &&
+      (board[move[0]][move[1]])._side != this.side);
   }
 
   findLegalMoves(board) {
     console.log(board);
-
     const x = this._x; // row
     const y = this._y; // column
     const v = this._vector // up/down
-
-    if(this._pristine) return this.filterOutBoardMoves([[x + v, y], [x + (2 * v), y]]);
-    return this.filterOutBoardMoves([[x + v, y]]);
+    let result = [];
+    if (this._pristine && board[x + v][y] == undefined) {
+      // allow double move if pawn is on its origin possition
+      // and possition in front of pawn is free
+      result = [
+        [x + v, y],
+        [x + (2 * v), y],
+      ]
+    } else {
+      result = [
+        [x + v, y],
+      ]
+    }
+    // filter moves and concat possible attacks
+    return Pawn.filterOccupiedPossition(Pawn.filterOutBoardMoves(result), board).concat(this.findLegalAttacks(board));
   }
 
 }
